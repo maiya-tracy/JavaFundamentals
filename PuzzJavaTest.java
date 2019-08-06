0e5ec61614bf3e62128b499ab985b4ef05acbba1
Maiya Tracy <maiya.tracy@gmail.com>
2019-08-06 13:06:00 -0700

    puzzles

--- PuzzJavaTest.java
+++ PuzzJavaTest.java
@@ -1,25 +1,35 @@
 import java.util.ArrayList;
+import java.util.*;
 import java.util.Collections;
 import java.util.Random;
 public class PuzzJavaTest {
 public static void main(String[] args) {
         PuzzleJava p = new PuzzleJava();
-        Integer[] sumArray = {3,5,1,2,7,9,8,13,25,32};
-        ArrayList<Integer> results_arr = p.sumAndGreaterThan10(sumArray);
-        System.out.println(results_arr);
+        // Integer[] sumArray = {3,5,1,2,7,9,8,13,25,32};
+        // ArrayList<Integer> results_arr = p.sumAndGreaterThan10(sumArray);
+        // System.out.println(results_arr);
 
+        // ArrayList<String> names = new ArrayList<String>();
+        // System.out.println(p.shuffleAndReturnNames(names));
 
+        // String[] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        // p.shuffleAndReturnAlpha(letters);
 
+        // ArrayList<Integer> results = p.generateRandomNumbers(55,100, 10);
+        // System.out.println(results);
+        // Collections.sort(results);
+        // p.showMinMax(results);
+        // System.out.println(results);
 
+        // System.out.println(p.randomString(5)); //5 characters long
+        // System.out.println(p.arrayOfStrings(5, 10)); //5 characters long, 10 strings
 
 
 
         // ArrayList<Integer> numbers = new ArrayList<Integer>();
         // numbers.add(5);
         // numbers.add(4);
-        // numbers.add(3);
-        // numbers.add(2);
-        // numbers.add(1);
+
         //
         // Collections.shuffle(numbers);
         // System.out.println(numbers); // [1, 5, 2, 4, 3]
--- PuzzleJava.java
+++ PuzzleJava.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class PuzzleJava {
+
 public ArrayList<Integer> sumAndGreaterThan10(Integer[] arr) {
         Integer sum = 0;
         ArrayList<Integer> results = new ArrayList<Integer>();
@@ -12,4 +16,66 @@ public ArrayList<Integer> sumAndGreaterThan10(Integer[] arr) {
         System.out.println(sum);
         return results;
 }
+public ArrayList<String> shuffleAndReturnNames(ArrayList<String> names) {
+        names.add("Nancy");
+        names.add("Jinichi");
+        names.add("Fujibayashi");
+        names.add("Momo");
+        names.add("Ishi");
+        Collections.shuffle(names);
+        System.out.println(names);
+        names.removeIf(name->(name.length() <= 5));
+        return names;
+}
+public void shuffleAndReturnAlpha(String[] letters) {
+        ArrayList<String> letterArray = new ArrayList<String>();
+        for (int i=0; i< letters.length; i++) {
+                letterArray.add(letters[i]);
+        }
+
+        Collections.shuffle(letterArray);
+        System.out.println(letterArray.get(25));
+        System.out.println(letterArray.get(0));
+        System.out.println(letterArray);
+        String vowels = "AEIOU";
+        if (vowels.contains(letterArray.get(0))) {
+                System.out.println("first letter is a vowel");
+        }
+}
+public ArrayList<Integer> generateRandomNumbers(Integer low, Integer high, Integer quantity) {
+        Random r = new Random();
+        ArrayList<Integer> results = new ArrayList<Integer>();
+        for (int i=0; i < quantity; i++) {
+                results.add(r.nextInt(high-low) + low);
+        }
+        return results;
+}
+public void showMinMax(ArrayList<Integer> results) {
+        System.out.println("Min: " + results.get(0) + " Max: " + results.get(results.size() - 1 ));
+}
+public String randomString(int length) {
+        StringBuilder s = new StringBuilder();
+        Random r = new Random();
+        while (s.length() < 5 ) {
+                char c = (char) (r.nextInt(26) + 'a');
+                s.append(c);
+        }
+        return s.toString();
+}
+public ArrayList<String> arrayOfStrings(int length, int quantity) {
+        ArrayList<String> results = new ArrayList<String>();
+        for (int i=0; i < quantity; i++) {
+                results.add(randomString(length));
+        }
+        return results;
+}
+
+
+
+
+
+
+
+
+
 }
